9766a734e58a06c4b1f1bf2f92989dc04292bd49
pvicugna11 <pvicugna11@gmail.com>
2021-12-14 15:31:20 +0900

    Follow the Player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody enemyRb;
+    private GameObject player;
+    public float speed = 3.0f;
+
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        enemyRb.AddForce(lookDirection * speed);
+    }
+}
